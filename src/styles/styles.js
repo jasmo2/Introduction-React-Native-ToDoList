52d56e99d22c27df294b8ef2ba8a7c93d45ab924
jasmo2 <jasmo2.com@gmail.com>
2016-09-27 17:26:18 -0500

    refactor styles

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,7 +3,12 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 
-    navigator: {flex: 1},
+    navigator: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'gray',
+    },
 
     container: {
         flex: 1,
